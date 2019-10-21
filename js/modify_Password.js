1032fd62c66af36180f9c2740841b8947535ba4c
肖龙涛 <319792690@qq.com>
2019-10-21 14:02:23 +0800

    老师端

---
+++ js/modify_Password.js
@@ -0,0 +1,9 @@
+
+$(".user_Password>div>span:nth-child(2)").click(function(){
+	$(".father_modify").css("display","block")
+});
+
+$('.qx').click(function(){
+	$(".father_modify").css("display","none")
+	// alert(11)
+})
